b65d8e1249814807caeab19afeab9d4530852755
Gabriel <gabgithub05@gmail.com>
2022-02-24 11:58:59 +0100

    Update fizzBuzz.cpp

--- fizzBuzz/fizzBuzz.cpp
+++ fizzBuzz/fizzBuzz.cpp
@@ -5,7 +5,7 @@
 #include <iostream>
 
 int main() {
-  for(int i = 1; i < 100; i++) {  
+  for(int i = 1; i < 100; i++) {   // don't start with i=0 because that's a mathematical error, and it don't use exception handling
     std::cout << i;
     if(i % 3 == 0){   // check if i is evenly divisible by 3
       std::cout << " Fizz";
